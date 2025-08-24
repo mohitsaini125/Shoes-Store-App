b3ae9035b074022533c18d5424f39fc5386dde5f
Mohit Saini <mohitmbdsaini@gmail.com>
2025-08-24 15:17:25 +0530

    third commit

--- app/index.jsx
+++ app/index.jsx
@@ -1,9 +1,8 @@
-import { LinearGradient } from "expo-linear-gradient";
 import {
   HeartIcon,
-  ListIcon,
   MagnifyingGlassIcon,
-  ShoppingBagIcon,
+  ShoppingBagOpenIcon,
+  TextOutdentIcon,
 } from "phosphor-react-native";
 import {
   FlatList,
@@ -12,172 +11,135 @@ import {
   StyleSheet,
   Text,
   TextInput,
-  TouchableOpacity,
   View,
 } from "react-native";
 import { categories, imgs } from "../constants/indexConstants";
 
-export default function HomePage() {
+export default function Homepage() {
   return (
-    <View style={styles.container1}>
-      <LinearGradient
-        colors={["#CECECE", "white"]}
-        start={{ x: 0, y: 0 }}
-        end={{ x: 1, y: 1 }}
-        style={styles.gradient}
-      />
-      <View style={styles.logo}>
+    <View>
+      <View style={styles.nikeLogoContainer}>
         <Image
           source={require("../assets/images/nike.png")}
-          style={styles.image1}
+          style={styles.nikeLogo}
         />
-        <View style={styles.logoRight}>
-          <HeartIcon size={32} color="black" />
-          <ShoppingBagIcon size={32} />
-          <ListIcon size={32} />
+        <View style={{ flexDirection: "row", gap: 20 }}>
+          <HeartIcon size={26} color="black" />
+          <ShoppingBagOpenIcon size={26} color="black" />
+          <TextOutdentIcon size={26} color="black" />
         </View>
       </View>
-      <View style={styles.searchBar}>
-        <TouchableOpacity style={{ alignSelf: "center", marginLeft: -5 }}>
-          <MagnifyingGlassIcon size={32} />
-        </TouchableOpacity>
-        <TextInput
-          placeholder="Search product"
-          placeholderTextColor={"lightgrey"}
-          style={styles.input}
-        />
+      <View>
+        <View style={styles.searchBar}>
+          <MagnifyingGlassIcon size={26} color="black" />
+          <TextInput placeholder="Search your shoes" />
+        </View>
       </View>
       <ScrollView
         horizontal
         showsHorizontalScrollIndicator={false}
-        style={styles.bannerScroll}
+        style={styles.banner}
       >
         {imgs.map((img, index) => (
-          <Image key={index} source={img} style={styles.banner} />
+          <Image source={img} key={index} style={styles.bannerImage} />
         ))}
       </ScrollView>
-      <View style={styles.dots}>
+      <View style={styles.dotContainer}>
         {[...Array(5)].map((_, index) => (
-          <View
-            key={index}
-            style={{
-              height: 10,
-              width: 10,
-              borderRadius: 100,
-              borderWidth: 1,
-              marginRight: 8,
-            }}
-          />
+          <View key={index} style={styles.dot} />
         ))}
       </View>
-
       <ScrollView
         horizontal
         showsHorizontalScrollIndicator={false}
-        style={styles.categories}
+        style={styles.categoriesContainer}
       >
         {categories.map((category, index) => (
-          <TouchableOpacity key={index} style={styles.categoryButton}>
-            <Text style={{ fontSize: 16 }}>{category}</Text>
-          </TouchableOpacity>
+          <Text style={styles.category} key={index}>
+            {category}
+          </Text>
         ))}
       </ScrollView>
-      <FlatList
-        contentContainerStyle={{
-          gap: 10,
-          marginHorizontal: 15,
-        }}
-        columnWrapperStyle={{ gap: 10 }}
-        numColumns={2}
-        data={imgs}
-        renderItem={function ({ item }) {
-          return <Image source={item} style={{ height: 150, width: 160 }} />;
-        }}
-      />
+      <View style={styles.shoeCard}>
+        <FlatList
+          data={imgs}
+          numColumns={2}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{ gap: 10 }}
+          columnWrapperStyle={{ gap: 10 }}
+          keyExtractor={(_, index) => index.toString()}
+          renderItem={({ item }) => (
+            <Image source={item} style={styles.shoeCardImage} />
+          )}
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container1: {
-    backgroundColor: "white",
-    flex: 1,
-  },
-  gradient: {
-    position: "absolute",
-    top: -60,
-    right: -80,
-    width: 300,
-    height: 300,
-    borderRadius: 200,
-  },
-  logo: {
-    backgroundColor: "#",
-    top: 70,
-    height: 80,
-    width: "100%",
-    alignSelf: "center",
+  nikeLogoContainer: {
     flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 30,
+    marginTop: 60,
+    alignItems: "center",
   },
-  image1: {
-    height: 80,
-    width: 80,
-    alignSelf: "left",
-    marginLeft: 30,
-  },
-  hearticon: {
-    position: "absolute",
-    top: 20,
-    right: 80,
-    alignSelf: "right",
-  },
-  logoRight: {
-    flexDirection: "row",
-    position: "absolute",
-    right: 20,
-    top: 20,
-    gap: 15,
+  nikeLogo: {
+    width: 60,
+    height: 40,
   },
   searchBar: {
-    marginHorizontal: "auto",
-    height: 60,
-    width: "90%",
-    marginTop: 65,
-    borderRadius: 10,
-    borderWidth: 1,
-    borderColor: "black",
-    paddingVertical: 0,
-    paddingLeft: 30,
     flexDirection: "row",
-  },
-  input: {
-    fontSize: 20,
-    marginLeft: 10,
+    alignItems: "center",
+    gap: 10,
+    paddingHorizontal: 20,
+    marginTop: 20,
+    borderWidth: 1,
+    marginHorizontal: 20,
+    borderRadius: 10,
   },
   banner: {
+    marginHorizontal: 20,
+    marginTop: 20,
+  },
+  bannerImage: {
     width: 320,
-    height: 120,
-    borderRadius: 20,
+    height: 100,
+    borderRadius: 10,
     marginRight: 20,
   },
-  bannerScroll: {
-    marginLeft: 20,
-    marginTop: 20,
-    height: 210,
-  },
-  dots: {
+  dotContainer: {
     flexDirection: "row",
-    marginHorizontal: "auto",
+    justifyContent: "center",
+    alignItems: "center",
     marginTop: 10,
+    gap: 10,
   },
-  categories: {
-    flexDirection: "row",
-    marginTop: 10,
-    marginBottom: 20,
-  },
-  categoryButton: {
+  dot: {
+    width: 8,
+    height: 8,
     borderWidth: 1,
-    borderRadius: 7,
-    marginHorizontal: 5,
+    borderRadius: 5,
+  },
+  categoriesContainer: {
+    marginTop: 20,
+    marginLeft: 20,
+  },
+  category: {
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    marginRight: 10,
+  },
+  shoeCard: {
+    marginTop: 20,
+    alignItems: "center",
+    height: 400,
+  },
+  shoeCardImage: {
+    width: 160,
+    height: 110,
   },
 });
--- constants/indexConstants.js
+++ constants/indexConstants.js
@@ -3,10 +3,17 @@ export const imgs = [
   require("../assets/images/banner2.png"),
   require("../assets/images/banner3.png"),
   require("../assets/images/banner1.png"),
+  require("../assets/images/banner2.png"),
+  require("../assets/images/banner3.png"),
   require("../assets/images/banner1.png"),
   require("../assets/images/banner2.png"),
   require("../assets/images/banner3.png"),
   require("../assets/images/banner1.png"),
+  require("../assets/images/banner2.png"),
+  require("../assets/images/banner3.png"),
+  require("../assets/images/banner1.png"),
+  require("../assets/images/banner2.png"),
+  require("../assets/images/banner3.png"),
 ];
 
 export const categories = [
